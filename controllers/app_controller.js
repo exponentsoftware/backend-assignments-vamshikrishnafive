fa131f7c7f9f9595e1cba22b16c3c7ac7668cc14
vamshikrishnafive <saikrishana9@gmail.com>
2021-06-28 22:34:24 +0530

    clear_out the log master

--- controllers/app_controller.js
+++ controllers/app_controller.js
@@ -5,7 +5,6 @@ export const FetchAllUserTask = async (req,res) => {
         const Alltasks = await UserTaskModel.find({});
         res.status(200).json(Alltasks);
     } catch (error) {
-        console.log(error);
         res.status(404).json({error: error})
     }
 };
@@ -16,32 +15,29 @@ export const FetchUserTaskById = async (req,res) => {
         const singleTask = await UserTaskModel.findById({_id: id});
         res.status(200).json(singleTask);
     } catch (error) {
-        console.log(error);
         res.status(404).json({error: error})
     }
 };
 
 export const createUserTask = async(req,res) => {
-    const { name, title, isTaskCompleted, category } = req.body;
-    const PostTask = new UserTaskModel({name, title, isTaskCompleted, category});
+    const { name, value, isTaskCompleted, category } = req.body;
+    const PostTask = new UserTaskModel({name, value, isTaskCompleted, category});
     try {
         await PostTask.save();
         res.status(200).json(PostTask);
     } catch (error) {
-        console.log(error);
         res.status(404).json({error: error})
     }
 };
 
 export const updateUserTask = async(req,res) => {
     const { id } = req.params
-    const { name, title, isTaskCompleted, category } = req.body;
-    const updateTask = await UserTaskModel.findByIdAndUpdate({ _id:id},{ name, title, isTaskCompleted, category},{new: true},)
+    const { name, value, isTaskCompleted, category } = req.body;
+    const updateTask = await UserTaskModel.findByIdAndUpdate({ _id:id},{ name, value, isTaskCompleted, category},{new: true},)
     try {
         await updateTask.save()
         res.status(200).json(updateTask);
     } catch (error) {
-        console.log(error);
         res.status(404).json({error: error})
     }
 };

ad47c87b8792bae841b311006811023cc53d73e0
vamshikrishnafive <saikrishana9@gmail.com>
2021-07-07 13:44:04 +0530

    auth file

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,34 +1,13 @@
-// export const auth = async(req, res, next) => {
-//     try {
-//         const token = req.headers.authorization.split(" ")[1];
-//         const isCustomAuth = token.length < 500;
-//         let decodedData;
-//         if(token && isCustomAuth) {
-//             decodedData = jwt.verify(token, secret);
-//             req.user_Id = decodedData?.Id;
-//         } else {
-//             decodedData = jwt.decode(token);
-//             req.user_Id = decodedData?.sub;
-//         }
-//         next();
-//     } catch (error) {
-//         console.log(error)
-//     }
-// };
-
-// export const isUser = (req, res, next) => {
-//     const {_id} = req.params;
-//     const userId = UserModel.find({_id})
-//     if(userId != _id) {
-//         return res.status(403).json({Error: "Access denied"});
-//     }
-//     req.userId = userId;
-//     next();
-// };
-
 // export const isAdmin = (req, res, next) => {
 //     if(req.user.role === 0) {
 //         return res.status(403).json({Error: "Admin resourse!...."});
 //     }
 //     next();
 // }
+
+exports.isAuth = (req, res, next) => {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/api/login')
+}

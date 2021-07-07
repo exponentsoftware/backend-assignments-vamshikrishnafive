1bb41fb73b54d4512a44d8c487a028a0b9b78de1
vamshikrishnafive <saikrishana9@gmail.com>
2021-07-07 13:45:10 +0530

    user

--- routers/user.js
+++ routers/user.js
@@ -1,13 +1,14 @@
 const express = require( 'express')
 
 const { signin, signup, login, register} = require('../controllers/user.js');
+const { isAuth } = require('../middleware/auth.js');
 
 const router = express.Router();
 
 router.get('/login', login)
 router.get('/register', register)
-router.post('/signin', signin);
-router.post('/signup', signup);
+router.post('/signin', isAuth ,signin);
+router.post('/signup', isAuth ,signup);
 
 
 module.exports = router;

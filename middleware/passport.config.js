0bf43d36284ed9b7dd392796abafc2c355c9a53f
vamshikrishnafive <saikrishana9@gmail.com>
2021-07-08 00:24:13 +0530

    Modified

---
+++ middleware/passport.config.js
@@ -0,0 +1,28 @@
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+const UserModel = require("../models/user");
+
+function initialPassport(passport) {
+    const authenticateUser = async(email, password, done) => {
+        const user = await UserModel.findOne({email})
+        if (!user) {
+            return done(null, false, { message: 'No user with that email_id' })
+        }
+        try {
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            } else {
+                done(null, false, { message: 'Password incorrect.!' })
+            }
+        } catch (error) {
+            return done(error)
+        }
+    }
+    passport.use(new LocalStrategy({ usernameField: 'email'}, authenticateUser))
+    passport.serializeUser((user, done) => {done(null, user.id)})
+    passport.deserializeUser((id, done) => {
+        UserModel.findById(id, (err, user) => { return done(err, user)})
+    })
+}
+
+module.exports = initialPassport

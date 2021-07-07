ac73afdb7dd28f80cb80589f2db99ed3bc06ad4f
vamshikrishnafive <saikrishana9@gmail.com>
2021-07-07 13:43:20 +0530

    passport

--- passport.config.js
+++ passport.config.js
@@ -1,27 +1,31 @@
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
+const UserModel = require("./models/user");
 
 function initialPassport(passport, getUserbyEmail, getUserById) {
     // console.log(getUserByEmail, getUserById)
     const authenticateUser = async(email, password, done) => {
-        const user = getUserbyEmail(email)
-        // console.log(user/)
-        // if (user === null) {
-        //     return done(null, false, { message: 'No user with that email_id' })
-        // }
-        // try {
-        //     if (await bcrypt.compare(password, user.password)) {
-        //         return done(null, user)
-        //     } else {
-        //         done(null, false, { message: 'Password incorrect.!' })
-        //     }
-        // } catch (error) {
-        //     return done(error)
-        // }
+        const user = await UserModel.findOne({email})
+        // console.log(user.id)
+        // console.log(password, user.password)
+        if (user === null) {
+            return done(null, false, { message: 'No user with that email_id' })
+        }
+        try {
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            } else {
+                done(null, false, { message: 'Password incorrect.!' })
+            }
+        } catch (error) {
+            return done(error)
+        }
     }
-    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
-    // passport.serializeUser((user, done) => { })
-    // passport.deserializeUser((id, done) => { })
+    passport.use(new LocalStrategy({ usernameField: 'email'}, authenticateUser))
+    passport.serializeUser((user, done) => {done(null, user.id)})
+    passport.deserializeUser((id, done) => {
+        UserModel.findById(id, (err, user) => { return done(err, user)})
+    })
 }
-
+    
 module.exports = initialPassport

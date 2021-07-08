6f8d9a32075b4fd118a14534349d3b3ac726ffe1
vamshikrishnafive <saikrishana9@gmail.com>
2021-07-08 21:55:40 +0530

    modified for the pagination

--- routers/task.js
+++ routers/task.js
@@ -11,20 +11,18 @@ const { adminUser,
         completedTask,
         deleteUserTodo } = require('../controllers/task');
 
-const { isAuth } = require('../middleware/auth');
-const paginationResult = require('../middleware/Pagination');
-const UserTaskModel = require('../models/task');
+// const  } = require('../middleware/auth');
 const router = express.Router();
 
-router.get('/todo/admin', isAuth, paginationResult(UserTaskModel), adminUser)
-router.get('/todo/get/:user_id', isAuth, getAllUserTodo);
-router.post('/todo/create/:user_id', isAuth, createUserTodo);
-router.get('/todo/list/:user_id', isAuth, sortByDate); // this api take query in url ex: url?order=1| -1
-router.get('/todo/search/:user_id', isAuth, searchByTitle); // this api take query in url ex: url?tilte=you
-router.get('/todo/categorySearch/:user_id', isAuth, searchByCategory); //not working need to fixed
-router.get('/todo/:user_id', isAuth, getSigleTodo);
-router.patch('/todo/update/:user_id',isAuth, updateUserTodo);
-router.patch('/todo/takComplete/:user_id', isAuth, completedTask);
-router.delete('/todo/delete/:user_id', isAuth, deleteUserTodo);
+router.get('/todo/admin', adminUser)
+router.get('/todo/get/:user_id', getAllUserTodo);
+router.post('/todo/create/:user_id', createUserTodo);
+router.get('/todo/list/:user_id', sortByDate); // this api take query in url ex: url?order=1| -1
+router.get('/todo/search/:user_id', searchByTitle); // this api take query in url ex: url?tilte=you
+router.get('/todo/categorySearch/:user_id', searchByCategory); //not working need to fixed
+router.get('/todo/:user_id', getSigleTodo);
+router.patch('/todo/update/:user_id', updateUserTodo);
+router.patch('/todo/takComplete/:user_id', completedTask);
+router.delete('/todo/delete/:user_id', deleteUserTodo);
 
 module.exports = router;

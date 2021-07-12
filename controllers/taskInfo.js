4a9b84c901d342b0f00008fb1c76b51028c068ea
vamshikrishnafive <saikrishana9@gmail.com>
2021-07-12 20:59:15 +0530

    sortByvies

--- controllers/taskInfo.js
+++ controllers/taskInfo.js
@@ -28,3 +28,15 @@ exports.rating = async(req, res) => {
     const updatedTask = await taskInfoModel.findOneAndUpdate(id, rating, {new: true});
     res.status(200).json(updatedTask)
 };
+
+exports.sortByMostView = async(req, res) => {
+    try {
+        const views = await taskInfoModel.aggregate([
+            {$group: { _id: "views", count: { $sum: 1}}},
+            {$sort: {count: 1}}
+        ]) 
+        res.status(200).json(MaxTask)
+    } catch (error) {
+        res.status(404).json({ error: error.message });
+    }
+}
